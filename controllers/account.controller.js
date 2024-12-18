90d05773a57fd70680d77c2e5f337f1dbac6b5d1
yamakunson <92131933+yamakunson@users.noreply.github.com>
2024-12-18 21:16:55 +0700

    18/12

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -109,20 +109,27 @@ module.exports.changePassword = async (req, res) => {
 };
 module.exports.forgotPassword = async (req, res) => {
   const { email } = req.body;
+  console.log(email);
   try {
+    console.log(1);
     var account = await Account.findOne({ email });
     if (!account) {
       return res.status(404).json({ error: "Account not found" });
     }
+    console.log(2);
     const newpassword = generate.generateRandomNumber(8);
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(newpassword, salt);
-    account = await Account.findByIdAndUpdate(id, {
-      password: hashedPassword,
-      passwordChangedAt: Date.now()
+    console.log(3);
+    account = await Account.findByIdAndUpdate(account._id, {
+      password: hashedPassword
     });
+    console.log("New password: " + newpassword);
     const mailOptions = {
-      from: process.env.GMAIL, // Your email address
+      from: {
+        name: 'BKPetCare',
+        address: process.env.GMAIL
+      },
       to: email,
       subject: 'Mật khẩu mới cho BKPetCare',
       text: `Mật khẩu mới của bạn là ${newpassword}. Đừng chia sẻ mật khẩu này với bất kỳ ai khác.`
